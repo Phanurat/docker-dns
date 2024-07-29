162d41e24dc549ebe36a2d09aae0b197859a773b
Panurat <paunavt5@gmail.com>
2024-07-29 14:52:01 +0700

    add test serve

--- duckdns/duckdns.sh
+++ duckdns/duckdns.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=YOUR_DOMAIN&token=YOUR_TOKEN&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=phex-phanurat&token=51b5b569-1e67-48a4-981c-f2dc0e23b7b1&ip=" | curl -k -o ~/duckdns/duck.log -K -
